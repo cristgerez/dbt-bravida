0c921b6b2db5ef18692f4a7113476a2d98717e6d
gerezcristobal@gmail.com <gerezcristobal@gmail.com>
2025-03-19 20:11:22 +0000

    Modifying query

--- models/mart/facts_taxi_trips.sql
+++ models/mart/facts_taxi_trips.sql
@@ -12,20 +12,12 @@ with base as (
         extract(hour from tpep_pickup_datetime) as hour_of_day,
         extract(dayofweek from tpep_pickup_datetime) as day_of_week
     from {{ ref("stg_raw_taxi_trips") }}
-),
-
-aggregated as (
-    select
-        hour_of_day,
-        day_of_week,
-        avg(trip_duration_minutes) as avg_trip_duration_minutes
-    from base
-    group by hour_of_day, day_of_week
 )
 
 select
     day_of_week,
     hour_of_day,
-    avg_trip_duration_minutes
-from aggregated
+    avg(trip_duration_minutes) as avg_trip_duration_minutes
+from base
+group by hour_of_day, day_of_week
 order by day_of_week, hour_of_day
